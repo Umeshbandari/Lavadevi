6e7d71771008186d99b9f0f6b7fe00826787da7b
Umeshbandari <umeshbandari19@gmail.com>
2026-04-18 15:25:56 +0530

    update2.0

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRHmhR6tJ6zgzLIQ6gqdmljR1fUl3kPXs",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "lavadevi-43a98.firebaseapp.com",
   projectId: "lavadevi-43a98",
   storageBucket: "lavadevi-43a98.firebasestorage.app",
